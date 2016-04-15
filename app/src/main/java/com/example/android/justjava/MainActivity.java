a4d4e1266c0ef7dc54eba2a2cffee43e9548ff9c
Prateesh Goswami <prateeshgoswami@gmail.com>
2016-04-14 18:14:47 -0700

    logic introduced

--- app/src/main/java/com/example/android/justjava/MainActivity.java
+++ app/src/main/java/com/example/android/justjava/MainActivity.java
@@ -1,21 +1,10 @@
 package com.example.android.justjava;
 
-import android.content.Intent;
-import android.net.Uri;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
-import android.text.Editable;
-
-import android.support.v7.app.ActionBarActivity;
-import android.util.Log;
 import android.view.View;
-import android.widget.CheckBox;
-import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
-
-import java.text.NumberFormat;
-import java.util.jar.Attributes;
 
 /**
  * This app displays an order form to order coffee.
@@ -30,93 +19,44 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
     }
 
-    int quantity = 1;
+    int calorie = 0;
+    int number;
 
-    /**
-     * Create summary
-     *
-     * @param name            of the customer
-     * @param price
-     * @param addWhippedCream w if the whipped cream is added or not
-     * @param haschoco        if it has chocolate
-     * @return text summary
-     */
-    private String createSummary(String name, double price, boolean addWhippedCream, boolean haschoco) {
 
-        String priceMessage = "Name :-- " + name;
-        priceMessage += " \nHas whipped cream  =" + addWhippedCream;
-        priceMessage += " \nHas chocolate  =" + haschoco;
-        priceMessage += "\n Quantity = " + quantity;
-        priceMessage += "\n After adding tax @6.5% ";
-        priceMessage += "\n Total =  $ " + price;
-        priceMessage += "\n Thank You!!!";
-        return priceMessage;
+    public int calculateCalorie(int number) {
+       int bal = 1500 - number;
+        return bal;
 
     }
 
-    /**
-     * Calculate price
-     *
-     * @param haswhipped w if the whipped cream is added or not
-     * @param haschoco   if it has chocolate
-     * @return price
-     */
-    public double calculatePrice(boolean haswhipped, boolean haschoco) {
 
-        double basePrice = 5;
-        if (haswhipped) {
-            basePrice = basePrice + 1;
-        }
-        if (haschoco) {
-            basePrice = basePrice + 2;
-        }
-        basePrice = basePrice * quantity;
-        double tax = .065;
-       double total = basePrice + (basePrice * tax);
-        return total;
 
-    }
-
-    /**
-     * This method is called when the order button is clicked.
-     */
-    public void submitOrder(View view) {
-        EditText text = (EditText) findViewById(R.id.editText);
-        String name = text.getText().toString();
-        CheckBox whippedCream = (CheckBox) findViewById(R.id.checkbox_whip);
-        CheckBox chocolate = (CheckBox) findViewById(R.id.checkbox_choco);
-        boolean haswhipped = whippedCream.isChecked();
-        boolean haschoco = chocolate.isChecked();
-        double price = calculatePrice(haswhipped, haschoco);
-        String message = createSummary(name, price, haswhipped, haschoco);
-        displayPrice(message);
-    }
     /**
      * This method is called when the plus button is clicked.
      */
     public void increment(View view) {
-        if (quantity == 100) {
+        if (calorie == 1500) {
             // Show an error message as a toast
-            Toast.makeText(this, "You cannot have more than 100 coffees", Toast.LENGTH_SHORT).show();
+            Toast.makeText(this, "You cannot have more calories", Toast.LENGTH_SHORT).show();
             // Exit this method early because there's nothing left to do
             return;
         }
-        quantity = quantity + 1;
-        display(quantity);
+        calorie = calorie + 10;
+        display(calculateCalorie(calorie));
     }
 
     /**
      * This method is called when the minus button is clicked.
      */
     public void decrement(View view) {
-        if (quantity == 1) {
+        if (calorie == 1) {
             // Show an error message as a toast
             Toast.makeText(this, "You cannot have less than 1 coffee", Toast.LENGTH_SHORT).show();
             // Exit this method early because there's nothing left to do
             return;
         }
-        quantity = quantity - 1;
-        display(quantity);
+        calorie = calorie - 10;
+        display(calculateCalorie(calorie));
     }
 
     /**
@@ -128,12 +68,5 @@ public class MainActivity extends AppCompatActivity {
         quantityTextView.setText("" + number);
     }
 
-    /**
-     * This method displays the given price on the screen.
-     */
 
-    private void displayPrice(String message) {
-        TextView priceTextView = (TextView) findViewById(R.id.price_text_view);
-        priceTextView.setText(message);
-    }
 }
